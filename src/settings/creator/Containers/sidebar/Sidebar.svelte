72838c4b42a695e100fae4b151c9af270f22c07a
Matt Hills <mph8344@rit.edu>
2025-10-10 00:05:56 -0400

    fix styling of cleaned up creator and move save/cancel buttons

--- src/settings/creator/Containers/sidebar/Sidebar.svelte
+++ src/settings/creator/Containers/sidebar/Sidebar.svelte
@@ -3,62 +3,16 @@
     import CreatorTitle from "../../CreatorTitle.svelte";
     import type { CreatorSection } from "../../creator.types";
     import Sections from "./Sections.svelte";
-    import { ButtonComponent } from "obsidian";
-    import { createEventDispatcher } from "svelte";
     import { getContext } from 'svelte';
 
     export let sections: CreatorSection[];
     export let selected: Writable<CreatorSection>;
-
-    const store = getContext("store");
-    const { valid } = store;
-
-    const dispatch = createEventDispatcher<{ cancel: null, save: null }>();
-    const cancel = (node: HTMLDivElement) => {
-        new ButtonComponent(node)
-            .setButtonText("Cancel")
-            .onClick(() => {
-                dispatch("cancel");
-            });
-    };
-
-    const setDisabled = (btn: ButtonComponent, valid: boolean) => {
-        const toolTip = valid ? '' : 'Calendar setup incomplete'
-
-        btn.setDisabled(!valid)
-        btn.setTooltip(toolTip);
-    }
-
-
-    let saveButton: ButtonComponent | null = null;
-    let saveNode: HTMLElement | null = null;
-
-    const save = (node: HTMLElement) => {
-        saveNode = node;
-        saveButton = new ButtonComponent(node)
-            .setButtonText("Save")
-            .setCta()
-            .onClick(() => dispatch("save"));
-
-        setDisabled(saveButton, $valid);
-    };
-
-    $: if (saveButton) {
-        setDisabled(saveButton, $valid)
-    }
-
-
 </script>
 
 <div class="vertical-tab-header">
     <CreatorTitle />
     <div class="vertical-tab-header-group">
         <Sections {selected} {sections} />
-    </div>
-
-    <div class="bottom">
-        <div class="cancel" use:cancel />
-        <div use:save />
     </div>
 </div>
 
--- src/settings/creator/Creator.svelte
+++ src/settings/creator/Creator.svelte
@@ -14,10 +14,15 @@
     import Sidebar from "./Containers/sidebar/Sidebar.svelte";
     import Seasonal from "./Containers/seasonal/Seasonal.svelte";
     import Locations from "./Containers/locations/Locations.svelte";
+    import { getContext } from 'svelte';
+    import { createEventDispatcher } from 'svelte';
 
     const mobile = Platform.isMobile;
 
     let ready = mobile;
+
+    const store = getContext("store");
+    const { valid } = store;
 
     onMount(() => {
         ready = true;
@@ -27,40 +32,81 @@
 
     export let color: string | null = null;
     export let top: number;
+
+    const dispatch = createEventDispatcher<{ cancel: null, save: null }>();
+
+    let saveButton: ButtonComponent | null = null;
+    let saveNode: HTMLElement | null = null;
+
+    const setDisabled = (btn: ButtonComponent, valid: boolean) => {
+        const toolTip = valid ? '' : 'Calendar setup incomplete'
+
+        btn.setDisabled(!valid)
+        btn.setTooltip(toolTip);
+    }
+
+    const cancel = (node: HTMLDivElement) => {
+        new ButtonComponent(node)
+            .setButtonText("Cancel")
+            .onClick(() => {
+                dispatch("cancel");
+            });
+    };
+
+    const save = (node: HTMLElement) => {
+        saveNode = node;
+        saveButton = new ButtonComponent(node)
+            .setButtonText("Save")
+            .setCta()
+            .onClick(() => dispatch("save"));
+
+        setDisabled(saveButton, $valid);
+    };
+
+    $: if (saveButton) {
+        setDisabled(saveButton, $valid)
+    }
+
 </script>
 
 {#if Platform.isTablet || Platform.isDesktop}
     <Sidebar {selected} sections={[...SettingsSections]} on:cancel on:save />
-    <div class="vertical-tab-content-container {$selected.toLowerCase()}">
+    <div class="creator-content-wrapper {$selected.toLowerCase()}">
         <History></History>
-        <div class="vertical-tab-content">
-            {#if $selected == "General"}
-                <General />
-            {/if}
-            {#if $selected == "Dates"}
-                <Dates />
-            {/if}
-            {#if $selected === "Eras"}
-                <Eras />
-            {/if}
-            {#if $selected === "Seasons & weather"}
-                <Seasonal />
-            {/if}
-            <!--  -->
-            {#if $selected === "Locations"}
-                <Locations />
-            {/if}
-            {#if $selected == "Events"}
-                <Events />
-            {/if}
-            {#if $selected == "Celestial bodies"}
-                <Celestials />
-            {/if}
+        <div class="tab-content-wrapper">
+            <div class="vertical-tab-content calendar-editor">
+                {#if $selected == "General"}
+                    <General />
+                {/if}
+                {#if $selected == "Dates"}
+                    <Dates />
+                {/if}
+                {#if $selected === "Eras"}
+                    <Eras />
+                {/if}
+                {#if $selected === "Seasons & weather"}
+                    <Seasonal />
+                {/if}
+                {#if $selected === "Locations"}
+                    <Locations />
+                {/if}
+                {#if $selected == "Events"}
+                    <Events />
+                {/if}
+                {#if $selected == "Celestial bodies"}
+                    <Celestials />
+                {/if}
+            </div>
+                <div class='save-buttons'>
+                    <div use:cancel/>
+                    <div use:save/>
+                </div>
+
         </div>
     </div>
 {:else}
     <div
-        class="calendarium-creator"
+        class="calendarium-creator calendarium-creator-mobile"
         style="--creator-background-color: {color}; --top: {top}px;"
     >
         {#if ready}
@@ -80,11 +126,11 @@
 {/if}
 
 <style>
-    .calendarium-creator,
+    /* .calendarium-creator,
     .calendarium-creator .calendarium-creator-inner,
     .calendarium-creator .calendarium-creator-app {
         background-color: var(--creator-background-color);
-    }
+    } */
     .calendarium-creator-app {
         overflow: auto;
         height: 100%;
@@ -92,5 +138,31 @@
     .vertical-tab-content {
         padding: var(--size-4-8);
         padding-top: 0;
+    }
+    .calendarium-creator-mobile {
+        padding: var(--size-4-8) !important;
+    }
+    .calendar-editor {
+        flex-grow: 1;
+    }
+
+    .creator-content-wrapper {
+        display: flex;
+        flex-direction: column;
+        overflow: hidden;
+        flex-grow: 1;
+    }
+
+    .tab-content-wrapper {
+        flex-grow: 1;
+        display: grid;
+        grid-template-rows: 1fr auto;
+        overflow: hidden;
+    }
+    .save-buttons {
+        display: flex;
+        justify-content: flex-end;
+        padding: var(--size-4-4);
+        gap: .5rem;
     }
 </style>
